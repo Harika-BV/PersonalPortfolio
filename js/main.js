a7f4617c21544115b8960410eb5695c405e61c5b
harika <harikabhaskaram31@gmail.com>
2020-05-24 23:05:20 +0530

    Improved

--- js/main.js
+++ js/main.js
@@ -293,7 +293,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 10) || 500;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
@@ -312,7 +312,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 300 - Math.random() * 100;
+  var delta = 200 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
@@ -322,7 +322,7 @@ TxtRotate.prototype.tick = function() {
   } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 500;
+    delta = 200;
   }
 
   setTimeout(function() {
@@ -336,7 +336,8 @@ window.onload = function() {
     var toRotate = elements[i].getAttribute('data-rotate');
     var period = elements[i].getAttribute('data-period');
     if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+		
+		new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
   // INJECT CSS
